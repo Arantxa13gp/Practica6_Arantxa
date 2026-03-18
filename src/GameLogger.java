26db520626f58a5ddfca06cfabff4c97c70ba31b
Arantxa13gp <arantxa13gp@gmail.com>
2026-03-18 18:02:02 +0100

    Tercer commit

--- src/GameLogger.java
+++ src/GameLogger.java
@@ -1,8 +1,8 @@
+import Combate.Combate;
 import Personajes.Personaje;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.management.modelmbean.InvalidTargetObjectTypeException;
+import java.io.*;
 
 public class GameLogger {
     public static void ficha(Personaje p) throws IOException {
@@ -11,6 +11,11 @@ public class GameLogger {
         bw.write("Clase:" + p.getClase());
         bw.write("Raza:" + p.getRaza());
         bw.write("Nivel:" + p.getNivel());
+        bw.write("Vida:" +  p.getPv());
+        bw.write("Ataque:" +  p.getAtq());
+        bw.write("Armadura:" +  p.getArm());
+        bw.write("Velocidad:" +  p.getVel());
+        bw.write("Resistencia:" +  p.getRes());
         bw.close();
     }
 
@@ -22,10 +27,27 @@ public class GameLogger {
                 bw.write("Clase:" + grupo[i].getClase());
                 bw.write("Raza:" + grupo[i].getRaza());
                 bw.write("Nivel:" + grupo[i].getNivel());
+                bw.write("Vida:" +  grupo[i].getPv());
+                bw.write("Ataque:" +  grupo[i].getAtq());
+                bw.write("Armadura:" +  grupo[i].getArm());
+                bw.write("Velocidad:" +  grupo[i].getVel());
+                bw.write("Resistencia:" +  grupo[i].getRes());
             }
+            //Falta ordenarlos por la velocidad
             bw.close();
     }
 
+    public static void verificarFicha() throws IOException{
 
+    }
 
+    public static void existePersonaje(File [] path, String nombre) throws IOException{
+       for (int i= 0; i < path.length; i++ ) {
+           BufferedReader br = new BufferedReader(new FileReader(path[i]));
+           
+           br.close();
+       }
+    }
+
+    //Ejercicio 6
 }
